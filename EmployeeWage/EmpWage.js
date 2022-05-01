f46555924ddbc3961a6f46c115555df3222eaa33
yburkul <yburkul123@gmail.com>
2022-05-01 15:23:35 +0530

    Added: Calculate Employee Wage

--- EmployeeWage/EmpWage.js
+++ EmployeeWage/EmpWage.js
@@ -1,11 +1,33 @@
 //UC1 - Emp is Present or absent
 const IS_ABSENT = 0;
 let empCheck = Math.floor(Math.random() * 2);
-if (empCheck == IS_ABSENT)
-{
+if (empCheck == IS_ABSENT) {
     console.log("Employee is ABSENT");
-} 
-else 
-{
+}
+else {
     console.log("Employee is PRESENT");
 }
+
+//UC2 Calculate Part Time And Full Time Employee Wage
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck) 
+{
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;
+}
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage: " + empWage);
