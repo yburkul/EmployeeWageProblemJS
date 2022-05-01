2656a1f9e58ef5f9293a5a72a2fee7fece44e74e
yburkul <yburkul123@gmail.com>
2022-05-01 21:50:39 +0530

    Added:Calculate Wage till number of working days and Hours per month

--- EmployeeWage/EmpWage.js
+++ EmployeeWage/EmpWage.js
@@ -1,9 +1,13 @@
-//UC3 Get workong Hours
+//UC5 Calculating Wages till number of working Days or Total Working Hours per Month
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const Num_OF_Working_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+let totalempHrs = 0;
+let totalWorkingDays = 0;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -16,13 +20,11 @@ function getWorkingHours(empCheck) {
     }
 }
 
-//UC4 Calculte Wages for a month assuming 20 working days in a month
-const Num_OF_Working_DAYS = 20;
-let totalempHrs = 0;
-for (let day = 0; day < Num_OF_Working_DAYS; day++)
+while (totalempHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < Num_OF_Working_DAYS)
 {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     totalempHrs += getWorkingHours(empCheck);
 }
 let empWage = totalempHrs * WAGE_PER_HOUR;
-console.log("Total Hrs: " +totalempHrs+ " Emp Wage: " + empWage);
+console.log("Total Days: " +totalWorkingDays+ " Total Hrs: " + totalempHrs + " Emp Wage: " + empWage);
