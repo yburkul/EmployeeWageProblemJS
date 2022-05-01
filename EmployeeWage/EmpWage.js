6416cc427b79adf2079ff8e45bac811052fb0190
yburkul <yburkul123@gmail.com>
2022-05-01 16:07:37 +0530

    Added:Get Working Hours

--- EmployeeWage/EmpWage.js
+++ EmployeeWage/EmpWage.js
@@ -1,13 +1,3 @@
-//UC1 - Emp is Present or absent
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 2);
-if (empCheck == IS_ABSENT) {
-    console.log("Employee is ABSENT");
-}
-else {
-    console.log("Employee is PRESENT");
-}
-
 //UC2 Calculate Part Time And Full Time Employee Wage
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -15,19 +5,20 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) 
-{
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
+//UC3 Get workong Hours
+console.log("Hours: " +empHrs);    
 console.log("Employee Wage: " + empWage);
