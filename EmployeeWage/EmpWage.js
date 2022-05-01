88aee2666a3c257c348df58c980ba8b56cbedcfb
yburkul <yburkul123@gmail.com>
2022-05-01 22:42:01 +0530

    Added: Daily Employee Wage

--- EmployeeWage/EmpWage.js
+++ EmployeeWage/EmpWage.js
@@ -8,6 +8,9 @@ const Num_OF_Working_DAYS = 20;
 const MAX_HRS_IN_MONTH = 160;
 let totalempHrs = 0;
 let totalWorkingDays = 0;
+let empHrs = 0;
+let dailyEmpHrs = 0;
+let dailyempwage = 0;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -20,11 +23,13 @@ function getWorkingHours(empCheck) {
     }
 }
 
-while (totalempHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < Num_OF_Working_DAYS)
+while (totalempHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= Num_OF_Working_DAYS)
 {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     totalempHrs += getWorkingHours(empCheck);
+    dailyEmpHrs += getWorkingHours(empCheck);
 }
 let empWage = totalempHrs * WAGE_PER_HOUR;
-console.log("Total Days: " +totalWorkingDays+ " Total Hrs: " + totalempHrs + " Emp Wage: " + empWage);
+dailyempwage = dailyEmpHrs * WAGE_PER_HOUR;
+console.log("Total Emp Wage: " + empWage+ "\nDaily Emp Wage: " +dailyEmpHrs);
