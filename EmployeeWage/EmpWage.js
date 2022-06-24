2d04ef0084863b76fc25a941b2ee5ff185629562
yburkul <yburkul123@gmail.com>
2022-06-24 22:21:21 +0530

    Added: Find The Total Wage And Total Hours Worked

--- EmployeeWage/EmpWage.js
+++ EmployeeWage/EmpWage.js
@@ -114,4 +114,28 @@ function totalDaysWorked(Num_OF_Working_DAYS,dailyWage)
 console.log("Number of Days Emp Worked: " + empDailyWageArr.reduce(totalDaysWorked,0));
 
 //UC 8 - Map Function
-console.log("Emp Wage Map Total Hrs: " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+console.log("Emp Wage Map Total Wages: " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+
+//UC 9- Arrow Functions
+const findTotal = (totalVal, dailyVal) => {
+    return totalVal + dailyVal;
+}
+let totalHours = Array.from(empDailyHrsMap.values())
+                      .filter(dailyHours => dailyHours > 0)
+                      .reduce(findTotal,0);
+let totalSalary = empDailyWageArr
+                  .filter(dailyWage => dailyWage > 0)
+                  .reduce(findTotal,0);
+console.log("Emp Wage with Arrow: " + " Total Hours: " + totalHours + " Total Wages: " +totalSalary);
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach( (value, key, map) => {
+    if (value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+console.log("Full Working Days: " + fullWorkingDays);
+console.log("Part Working Days: " + partWorkingDays);
+console.log("Non Working Days: " + nonWorkingDays);
